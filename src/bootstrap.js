97b31169632fa7f0b6613ad3f6e3602973443a5e
andylabonte <mr.andy.labonte@gmail.com>
2020-09-11 22:09:03 -0600

    fixed bugs and able to fetch recent posts

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,9 +4,12 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import thunk from 'redux-thunk';
+
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
 
 import "./style/main.scss";
 
@@ -17,7 +20,7 @@ function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-      <Switch>
+       <Switch>
             <Route path='/' exact component={Home}/>
             <Route path='/results' component={Results}/>
         </Switch>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 
 const rootReducer = combineReducers({
-  form
+    form
 });
 
 export default rootReducer;
