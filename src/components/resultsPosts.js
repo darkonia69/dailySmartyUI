9d2af15fe54afe3482d99c212507c2db7c81b8c3
andylabonte <mr.andy.labonte@gmail.com>
2020-09-30 21:19:17 -0600

    set up recent posts on results

---
+++ src/components/resultsPosts.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class ResultsPosts extends Component {
+    render() {
+        return (
+            <div className="results-posts">
+              <div className="results-posts_wrapper">
+                <ul className="results-posts_posts">
+                    results go here
+                  </ul>
+                </div> 
+             </div>
+            
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        state
+    }
+}
+
+export default connect(mapStateToProps)(ResultsPosts);
